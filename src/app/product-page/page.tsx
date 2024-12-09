6c566d09f0ec103a5d55e4b768ce23fe12790042
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2024-12-10 01:46:26 +0500

    Update page.tsx

--- src/app/product-page/page.tsx
+++ src/app/product-page/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Heart, ShoppingCart, Star, Search, Grid, List } from "lucide-react";
+import { Heart, ShoppingCart,  Search, Grid, List } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
