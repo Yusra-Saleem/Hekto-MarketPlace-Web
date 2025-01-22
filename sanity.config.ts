3256f5da323f7b0bbddd6d48333f9cd7d9716bee
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2025-01-22 10:30:17 +0500

    Hekto-Luxury Sofa & chairs Website

--- sanity.config.ts
+++ sanity.config.ts
@@ -15,14 +15,12 @@ import {structure} from './src/sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  projectId: projectId as string,
+  dataset: dataset as string,
   schema,
   plugins: [
     structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
-})
+});
+
