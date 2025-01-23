b40ef1749b8e976a31b0c337f3f7a7a52279b31f
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2025-01-23 18:44:34 +0500

    Delete src directory

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import products from './products'
-import order from './orders'
-import Review from './Review'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [products,order , Review],
-}
