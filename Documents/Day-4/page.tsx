1352ecf97cadd655900aa7dee9aaf8fce1ec451f
Yusra-Saleem <rayyan726.m@gmail.com>
2025-02-08 23:42:16 +0500

    document

---
+++ Documents/Day-4/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Hekto Marketplace</h1>
+            <p>This is the default page component.</p>
+        </div>
+    );
+};
+
+export default Page;
